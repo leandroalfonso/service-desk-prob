d7c97546e1a8ac50d5a95990f6ae193eb09a55b3
leandroalfonso <68873104+leandroalfonso@users.noreply.github.com>
2024-02-22 23:40:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require ('express');
-const fs = require ('fs');
 const app = express ();
 const PORT = process.env.PORT || 3000;
 const cors = require ('cors');
+const axios = require ('axios');
 
 app.use (express.json ());
 app.use (cors ());
@@ -19,119 +19,93 @@ app.get ('/lista', (req, res) => {
   res.sendFile (__dirname + '/lista.html');
 });
 
-app.get ('/detalhes/:id', (req, res) => {
+app.get ('/detalhes/:id', async (req, res) => {
   const id = parseInt (req.params.id);
-  fs.readFile ('https://idyllic-dolphin-d890b0.netlify.app/dados.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error ('Erro ao ler o arquivo JSON:', err);
-      res.status (500).json ({error: 'Erro interno do servidor'});
-      return;
+  try {
+    const response = await axios.get (
+      'https://idyllic-dolphin-d890b0.netlify.app/dados.json'
+    );
+    const jsonData = response.data;
+    const detalhes = jsonData.find (item => item.id === id);
+    if (detalhes) {
+      res.json (detalhes);
+    } else {
+      res.status (404).json ({error: 'Item não encontrado'});
     }
-    try {
-      const jsonData = JSON.parse (data);
-      const detalhes = jsonData.find (item => item.id === id);
-      if (detalhes) {
-        res.json (detalhes); // Retornar os detalhes do item em formato JSON
-      } else {
-        res.status (404).json ({error: 'Item não encontrado'});
-      }
-    } catch (error) {
-      console.error ('Erro ao fazer o parse do JSON:', error);
-      res.status (500).json ({error: 'Erro interno do servidor'});
-    }
-  });
+  } catch (error) {
+    console.error ('Erro ao buscar dados:', error);
+    res.status (500).json ({error: 'Erro interno do servidor'});
+  }
 });
 
-app.get ('/buscar/:termo', (req, res) => {
+app.get ('/buscar/:termo', async (req, res) => {
   const termo = req.params.termo.toLowerCase ();
-  fs.readFile ('https://idyllic-dolphin-d890b0.netlify.app/dados.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error ('Erro ao ler o arquivo JSON:', err);
-      res.status (500).json ({error: 'Erro interno do servidor'});
-      return;
-    }
-    try {
-      const jsonData = JSON.parse (data);
-      const resultados = jsonData.filter (item => {
-        return (
-          item.titulo.toLowerCase ().includes (termo) ||
-          item.conteudo.toLowerCase ().includes (termo) ||
-          item.id.toString ().includes (termo)
-        );
-      });
-      res.json (resultados);
-    } catch (error) {
-      console.error ('Erro ao fazer o parse do JSON:', error);
-      res.status (500).json ({error: 'Erro interno do servidor'});
-    }
-  });
+  try {
+    const response = await axios.get (
+      'https://idyllic-dolphin-d890b0.netlify.app/dados.json'
+    );
+    const jsonData = response.data;
+    const resultados = jsonData.filter (item => {
+      return (
+        item.titulo.toLowerCase ().includes (termo) ||
+        item.conteudo.toLowerCase ().includes (termo) ||
+        item.id.toString ().includes (termo)
+      );
+    });
+    res.json (resultados);
+  } catch (error) {
+    console.error ('Erro ao buscar dados:', error);
+    res.status (500).json ({error: 'Erro interno do servidor'});
+  }
 });
 
-app.put ('/editar/:id', (req, res) => {
+app.put ('/editar/:id', async (req, res) => {
   const id = parseInt (req.params.id);
   const novoConteudo = req.body.conteudo;
-
-  fs.readFile ('https://idyllic-dolphin-d890b0.netlify.app/dados.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error ('Erro ao ler o arquivo JSON:', err);
-      res.status (500).send ('Erro interno do servidor');
-      return;
+  try {
+    const response = await axios.get (
+      'https://idyllic-dolphin-d890b0.netlify.app/dados.json'
+    );
+    let jsonData = response.data;
+    const index = jsonData.findIndex (item => item.id === id);
+    if (index !== -1) {
+      // Atualiza o conteúdo do post
+      jsonData[index].conteudo = novoConteudo;
+      // Envia a requisição PUT para atualizar os dados
+      await axios.put (
+        `https://idyllic-dolphin-d890b0.netlify.app/editar/${id}`,
+        {conteudo: novoConteudo}
+      );
+      console.log (`Post ID: ${id} editado com sucesso`);
+      res.send ('Edição do post realizada com sucesso');
+    } else {
+      res.status (404).send ('Post não encontrado');
     }
-    try {
-      let jsonData = JSON.parse (data);
-      const index = jsonData.findIndex (item => item.id === id);
-      if (index !== -1) {
-        // Atualiza o conteúdo do post
-        jsonData[index].conteudo = novoConteudo;
-        // Escreve de volta no arquivo
-        fs.writeFile ('dados.json', JSON.stringify (jsonData), err => {
-          if (err) {
-            console.error ('Erro ao escrever no arquivo JSON:', err);
-            res.status (500).send ('Erro interno do servidor');
-            return;
-          }
-          console.log (`Post ID: ${id} editado com sucesso`);
-          res.send ('Edição do post realizada com sucesso');
-        });
-      } else {
-        res.status (404).send ('Post não encontrado');
-      }
-    } catch (error) {
-      console.error ('Erro ao fazer o parse do JSON:', error);
-      res.status (500).send ('Erro interno do servidor');
-    }
-  });
+  } catch (error) {
+    console.error ('Erro ao editar post:', error);
+    res.status (500).send ('Erro interno do servidor');
+  }
 });
 
-app.post ('/insere-json', (req, res) => {
+app.post ('/insere-json', async (req, res) => {
   const dados = req.body;
-  fs.readFile ('https://idyllic-dolphin-d890b0.netlify.app/dados.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error ('Erro ao ler o arquivo JSON:', err);
-      res.status (500).send ('Erro interno do servidor');
-      return;
-    }
-    let jsonContent = [];
-    if (data) {
-      try {
-        jsonContent = JSON.parse (data);
-      } catch (error) {
-        console.error ('Erro ao analisar o conteúdo JSON:', error);
-        res.status (500).send ('Erro interno do servidor');
-        return;
-      }
-    }
+  try {
+    const response = await axios.get (
+      'https://idyllic-dolphin-d890b0.netlify.app/dados.json'
+    );
+    let jsonContent = response.data;
     jsonContent.push (dados);
-    fs.writeFile ('https://idyllic-dolphin-d890b0.netlify.app/dados.json', JSON.stringify (jsonContent), err => {
-      if (err) {
-        console.error ('Erro ao escrever no arquivo JSON:', err);
-        res.status (500).send ('Erro interno do servidor');
-        return;
-      }
-      console.log ('Dados inseridos com sucesso:', dados);
-      res.send ('Dados inseridos com sucesso');
-    });
-  });
+    // Envia a requisição POST para inserir os dados
+    await axios.post (
+      'https://idyllic-dolphin-d890b0.netlify.app/insere-json',
+      dados
+    );
+    console.log ('Dados inseridos com sucesso:', dados);
+    res.send ('Dados inseridos com sucesso');
+  } catch (error) {
+    console.error ('Erro ao inserir dados:', error);
+    res.status (500).send ('Erro interno do servidor');
+  }
 });
 
 app.listen (PORT, () => {
