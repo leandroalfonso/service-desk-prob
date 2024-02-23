0386f4d201c0e001ef3f0575e8a207514b82624c
leandroalfonso <68873104+leandroalfonso@users.noreply.github.com>
2024-02-22 23:09:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require ('express');
 const fs = require ('fs');
 const app = express ();
 const PORT = 3000;
+const cors = require('cors');
 
-app.use (express.json ());
+
+
+app.use(express.json());
+
+app.use (cors ());
+
 
 app.get ('/', (req, res) => {
   res.sendFile (__dirname + '/index.html');
